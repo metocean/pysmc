a63c0e4806e88f286228a8ab5f5bc6cb2f7f6ca7
Tom Durrant <t.durrant@metocean.co.nz>
2017-10-26 13:48:23 +1300

    Removed unncessary examples

--- examples/glob005/run_gridgen.py
+++
@@ -1,7 +0,0 @@
-from SMCPy.gridgen import create_grid
-lonmin, lonmax =  0.0, 360.0
-latmin, latmax =  -75.0,75.0
-dlat = dlon = 0.05
-
-create_grid('glb005',latmin,latmax,lonmin,lonmax,dlat,dlon)
-
