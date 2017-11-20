fb44aad21891b991e825c74244ffed0f0d4f9b08
Tom Durrant <t.durrant@metocean.co.nz>
2017-11-20 23:29:40 +1300

    Added extra check for global grid

--- SMCPy/SMCGrid.py
+++ SMCPy/SMCGrid.py
@@ -189,7 +189,9 @@ class MatBathy(object):
         ww3_grid.inp)
         """
         # -- for size 1 [used to generate SMC]
-        self.globe = np.equal((self.lon[-1] - self.lon[0]), (360. - self.dlon))
+        if (np.equal((self.lon[-1] - self.lon[0]), (360. - self.dlon)) or
+           np.equal((self.lon[-1] - self.lon[0]), (360.))):
+            self.globe=True
         if self.globe:
             self.icol_beg, self.icol_end = glob_icol_beg, glob_icol_end
             self.jrow_beg, self.jrow_end = glob_jrow_beg, glob_jrow_end
